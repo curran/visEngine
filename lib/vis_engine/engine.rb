5a6677c278f693cc60bbd37647f8e5150a552f52
curran <curran.kelleher@gmail.com>
2015-05-13 18:33:33 -0700

    Got Rails asset pipeline basic test working

--- lib/vis_engine/engine.rb
+++ lib/vis_engine/engine.rb
@@ -2,9 +2,9 @@ module VisEngine
   class Engine < ::Rails::Engine
     isolate_namespace VisEngine
 
-    # Serve static assets in the 'public' directory through the engine.
-    initializer 'static assets' do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-    end
+    ## Serve static assets in the 'public' directory through the engine.
+    #initializer 'static assets' do |app|
+    #  app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+    #end
   end
 end
---
+++ non-rails/myJavaScript.js
@@ -0,0 +1,1 @@
+var myJavaScript = "The JS is loaded!";
